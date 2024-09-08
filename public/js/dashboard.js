9b344a3bb4e9efcb9bad4eb4f12a8ead5f0f7aa6
shanwc1972 <166612646+shanwc1972@users.noreply.github.com>
2024-09-08 11:50:15 +1000

    Adding new posts

---
+++ public/js/dashboard.js
@@ -0,0 +1,48 @@
+const newFormHandler = async (event) => {
+    event.preventDefault();
+  
+    const title = document.querySelector('#post-title').value.trim();
+    const content = document.querySelector('#post-content').value.trim();
+  
+    if (title && content) {
+        const JSONbody = JSON.stringify({ title, content});
+        console.log(JSONbody)
+        const response = await fetch(`/api/posts`, {
+            method: 'POST',
+            body: JSONbody,
+            headers: {
+            'Content-Type': 'application/json',
+            },
+        });
+  
+        if (response.ok) {
+            document.location.replace('/dashboard');
+        } else {
+            alert('Failed to create post');
+        }
+    }
+  };
+  
+  const delButtonHandler = async (event) => {
+    if (event.target.hasAttribute('data-id')) {
+      const id = event.target.getAttribute('data-id');
+  
+      const response = await fetch(`/api/posts/${id}`, {
+        method: 'DELETE',
+      });
+  
+      if (response.ok) {
+        document.location.replace('/dashboard');
+      } else {
+        alert('Failed to delete post');
+      }
+    }
+  };
+  
+  document
+    .querySelector('.new-post-form')
+    .addEventListener('submit', newFormHandler);
+  
+  document
+    .querySelector('.post-list')
+    .addEventListener('click', delButtonHandler);
